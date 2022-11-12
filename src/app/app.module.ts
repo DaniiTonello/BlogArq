595f09e3ae8d0370dcae07ac011f92c1e785f1e9
Daniela Tonello <daniela.a.tonello@gmail.com>
2022-11-12 17:57:54 -0300

    add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { ContactComponent } from './components/components/contact/contact.component';
+import { NavbarComponent } from './components/components/navbar/navbar.component';
+import { FooterComponent } from './components/components/footer/footer.component';
+import { InformationComponent } from './components/components/information/information.component';
+import { RendersComponent } from './components/components/renders/renders.component';
+import { WorksComponent } from './components/components/works/works.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactComponent,
+    InformationComponent,
+    NavbarComponent,
+    FooterComponent,
+    RendersComponent,
+    WorksComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
